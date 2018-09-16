9ba8e8019d641a68898d768ea3ae8dba8a3e3abd
kgy <kgy@DESKTOP-HKTL00R>
2018-09-16 18:32:45 +0900

    eahhe

--- Fleamarket/src/main/java/com/sp/payment/domain/MystoreDTO_gy.java
+++ Fleamarket/src/main/java/com/sp/payment/domain/MystoreDTO_gy.java
@@ -8,19 +8,44 @@ public class MystoreDTO_gy implements Serializable {
 	String Store_Name;
 	String Member_Email;
 	String Store_Contents;
-	
+	int follower_store_no;
 	
 	public MystoreDTO_gy(){}
 	
 	
 	
-	public MystoreDTO_gy(int store_No, String store_Name, String member_Email, String store_Contents) {
+	
+	
+	public MystoreDTO_gy(int store_No, String store_Name, String member_Email, String store_Contents,
+			int follower_store_no) {
 		super();
 		Store_No = store_No;
 		Store_Name = store_Name;
 		Member_Email = member_Email;
 		Store_Contents = store_Contents;
+		this.follower_store_no = follower_store_no;
 	}
+
+
+
+
+
+
+
+
+
+	public int getFollower_store_no() {
+		return follower_store_no;
+	}
+
+
+
+	public void setFollower_store_no(int follower_store_no) {
+		this.follower_store_no = follower_store_no;
+	}
+
+
+
 
 
 
